20b8bc52ad8a802ed70c1e2eed62be0294737967
Andrei Dogadaev <dogadlev@yandex.ru>
2023-03-01 21:16:48 +0300

    Initial commit.

---
+++ task1/task1.cpp
@@ -0,0 +1,114 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <exception>
+
+
+struct Query
+{
+	std::string name;
+	std::vector<std::string> column;
+	std::string from;
+	std::map<std::string, std::string> where;
+};
+
+class SqlSelectQueryBuilder
+{
+private:
+	Query query;
+	std::string getColumn()
+	{
+		std::string outColumn = "";
+		if (!query.column.empty())
+		{
+			size_t index = 1;
+			for (auto itr = query.column.begin(); itr != query.column.end(); ++itr)
+			{
+				outColumn += *itr;
+				if (index < query.column.size())
+				{
+					outColumn = outColumn + ", ";
+					++index;
+				}
+			}
+		}
+		else
+			outColumn = "*";
+		return outColumn;
+	}
+	std::string getFrom()
+	{
+		std::string outFrom = "";
+		if (!query.from.empty())
+		{
+			outFrom = "FROM " + query.from;
+		}
+		return outFrom;
+	}
+	std::string getWhere()
+	{
+		std::string outWhere = "";
+		if (!query.where.empty())
+		{
+			size_t index = 1;
+			for (auto itr = query.where.begin(); itr != query.where.end(); ++itr)
+			{
+				outWhere = outWhere + itr->first + "=" + itr->second;
+				if (index < query.where.size())
+				{
+					outWhere = outWhere + " AND ";
+					++index;
+				}
+			}
+			outWhere = "WHERE " + outWhere;
+		}
+		return outWhere;
+	}
+	void checkQuery()
+	{
+		if (query.column.empty() && query.where.empty() && query.from.empty())
+			throw std::runtime_error("Query is empty!");
+	}
+public:
+	SqlSelectQueryBuilder()
+	{
+		query.name = "SELECT";
+	}
+	SqlSelectQueryBuilder& AddColumn(const std::string&& columnName)
+	{
+		query.column.push_back(columnName);
+		return *this;
+	}
+	SqlSelectQueryBuilder& AddFrom(const std::string&& from)
+	{
+		query.from = from;
+		return *this;
+	}
+	SqlSelectQueryBuilder& AddWhere(const std::string&& key, const std::string&& val)
+	{
+		query.where.insert(std::pair<std::string, std::string>(key, val));
+		return *this;
+	}
+	std::string BuildQuery()
+	{
+		checkQuery();
+		std::string outQuery = "\"" + query.name + " " + getColumn() + " " + getFrom() + " " + getWhere() + ";\"";
+		return outQuery;
+	}
+};
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		SqlSelectQueryBuilder queryBuilder;
+		queryBuilder.AddColumn("name").AddColumn("phone");
+		queryBuilder.AddFrom("students");
+		queryBuilder.AddWhere("id", "42").AddWhere("name", "John");
+		std::cout << queryBuilder.BuildQuery();
+	}
+	catch (std::runtime_error& ex) { std::cout << ex.what() << std::endl; }
+
+	return 0;
+}
